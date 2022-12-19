106e1a909f118f4e556c0a2129c2be6ef89cc4d6
BambooCode98 <jw.thomas98@yahoo.com>
2022-12-19 10:49:03 -0600

    Updates

--- main.bundle.js
+++ main.bundle.js
@@ -186,7 +186,7 @@ eval("\n\n/* eslint-disable */\n\n/**\n * @param {string[]} pathComponents\n * @
   \**********************/
 /***/ ((module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n    if(true) {\n      // 1671468442389\n      var cssReload = __webpack_require__(/*! ../node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ \"./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js\")(module.id, {\"locals\":false});\n      module.hot.dispose(cssReload);\n      module.hot.accept(undefined, cssReload);\n    }\n  \n\n//# sourceURL=webpack://special_effects_gallery/./src/home.css?");
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n    if(true) {\n      // 1671468540760\n      var cssReload = __webpack_require__(/*! ../node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ \"./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js\")(module.id, {\"locals\":false});\n      module.hot.dispose(cssReload);\n      module.hot.accept(undefined, cssReload);\n    }\n  \n\n//# sourceURL=webpack://special_effects_gallery/./src/home.css?");
 
 /***/ }),
 
@@ -364,7 +364,7 @@ eval("module.exports = __webpack_require__.p + \"454785ae285f68b18d55.webM\";\n\
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("41eeea3c2086920f743a")
+/******/ 		__webpack_require__.h = () => ("83313d0649ca3b5ddb32")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
